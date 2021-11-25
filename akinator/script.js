675154894d8702584b1ecf06653a38f0e7279330
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-11-25 20:05:46 +0100

    1

--- akinator/script.js
+++ akinator/script.js
@@ -11,16 +11,23 @@ let provisional_question = finale_question;
 
 let shuffle = provisional_question.sort((a, b, c) => 0.5 - Math.random());
 let i=1;
-
+console.log(shuffle);
 while (!shuffle.length == []) {
-console.log(shuffle[0]);
 if (shuffle[0].includes("genre")) {
-  console.log('trier');
+  console.log(shuffle[0]);
   shuffle.shift()} 
   if (shuffle[0].includes("popular")) {
-    console.log('trier');
+    console.log(shuffle[0]);
     shuffle.shift()} 
-    shuffle.shift()
+    if (shuffle[0].includes("actor")) {
+      console.log(shuffle[0]);
+      shuffle.shift()} 
+      if (shuffle[0].includes("director")) {
+        console.log(shuffle[0]);
+        shuffle.shift()} 
+        if (shuffle[0].includes("2000s")) {
+          console.log(shuffle[0]);
+          shuffle.shift()} 
 }
 
 
