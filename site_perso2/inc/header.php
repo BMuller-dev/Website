d592c47892b483d3db11eed3a6a8fdd03532bc37
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-11-26 22:23:41 +0100

    1

--- site_perso2/inc/header.php
+++ site_perso2/inc/header.php
@@ -6,21 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link href="assets/css/main.css" rel="stylesheet">
-    <script src=" assets/js/main.js">
+    <script src="assets/js/main.js">
     </script>
 </head>
 
 <body>
     <header>
-        <div class="wrapper">
-            <nav>
-                <a class="logo" href="#">BM</a>
-                <a class="categorie about" href="#">About</a>
-                <a class="categorie galerie" href="#">Galerie</a>
-                <a class="categorie contact" href="#">Contact</a>
-                <a class="cv" href="#">CV</a>
-            </nav>
-
-        </div>
+        <nav>
+            <div class="logo">
+                <a href="#">BM</a>
+            </div>
+            <div class="categorie">
+                <a class="about" href="#">About</a>
+                <a class="galerie" href="#">Galerie</a>
+                <a class="contact" href="#">Contact</a>
+            </div>
+            <div class="cv">
+                <a href="#">CV</a>
+            </div>
+        </nav>
     </header>
     <main>
