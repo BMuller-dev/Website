38933fba95d223f268a9a8e8280b4a05cbbc1187
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2021-11-29 16:31:13 +0100

    1

--- site_perso2/inc/header.php
+++ site_perso2/inc/header.php
@@ -6,33 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link href="assets/css/main.css" rel="stylesheet">
-    <script src="assets/js/main.js">
-        < /head>
+    <script src="assets/js/main.js"></script>
+</head>
 
-        <
-        body >
-            <
-            header >
-            <
-            nav >
-            <
-            div class = "logo" >
-            <
-            a href = "#" > BM < /a> <
-            /div> <
-            div class = "categorie" >
-            <
-            a class = "about"
-        href = "#" > About < /a> <
-            a class = "galerie"
-        href = "#" > Galerie < /a> <
-            a class = "contact"
-        href = "#" > Contact < /a> <
-            /div> <
-            div class = "cv" >
-            <
-            a href = "#" > CV < /a> <
-            /div> <
-            /nav> <
-            /header> <
-            main >
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="#"> BM </a></div>
+            <div class="categorie">
+                <a class="about" href="#"> About </a> <a class="galerie" href="#"> Galerie </a> <a class="contact" href="#"> Contact </a> </div>
+            <div class="cv">
+                <a href="#"> CV </a></div>
+        </nav>
+    </header>
+    <main>
