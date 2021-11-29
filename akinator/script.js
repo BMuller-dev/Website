e13bb5ed7d4b6aaece7d5b668d06b559066d6da7
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2021-11-29 15:30:48 +0100

    1

--- akinator/script.js
+++ akinator/script.js
@@ -2,20 +2,18 @@ import {
   movies
 } from "./movie.js";
 
-const finale_question = [
+const final_questions = [
   "What is the genre of your movie ?",
   "Is your movie from after the 2000s ?",
   "Who is the main actor ?",
   "Is the director one of these proposals?",
   "Is your movie popular ?"
 ];
-let provisional_question = finale_question;
+let provisional_questions = final_questions;
 
-let shuffle = provisional_question.sort((a, b, c) => 0.5 - Math.random());
-console.log(shuffle);
+let shuffle = provisional_questions.sort((a, b, c) => 0.5 - Math.random());
 
 
-console.log(shuffle);
 while (!shuffle.length == []) {
   if (shuffle[0].includes("genre")) {
     console.log(shuffle[0]);
