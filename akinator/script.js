167df5d69c58584bc180de207f45c803e893c14a
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2021-11-29 13:53:01 +0100

    1

--- akinator/script.js
+++ akinator/script.js
@@ -1,4 +1,6 @@
-import { movies } from "./movie.js";
+import {
+  movies
+} from "./movie.js";
 
 const finale_question = [
   "What is the genre of your movie ?",
@@ -10,24 +12,29 @@ const finale_question = [
 let provisional_question = finale_question;
 
 let shuffle = provisional_question.sort((a, b, c) => 0.5 - Math.random());
-let i=1;
+console.log(shuffle);
+
+
 console.log(shuffle);
 while (!shuffle.length == []) {
-if (shuffle[0].includes("genre")) {
-  console.log(shuffle[0]);
-  shuffle.shift()} 
+  if (shuffle[0].includes("genre")) {
+    console.log(shuffle[0]);
+    shuffle.shift()
+  }
   if (shuffle[0].includes("popular")) {
     console.log(shuffle[0]);
-    shuffle.shift()} 
-    if (shuffle[0].includes("actor")) {
-      console.log(shuffle[0]);
-      shuffle.shift()} 
-      if (shuffle[0].includes("director")) {
-        console.log(shuffle[0]);
-        shuffle.shift()} 
-        if (shuffle[0].includes("2000s")) {
-          console.log(shuffle[0]);
-          shuffle.shift()} 
+    shuffle.shift()
+  }
+  if (shuffle[0].includes("actor")) {
+    console.log(shuffle[0]);
+    shuffle.shift()
+  }
+  if (shuffle[0].includes("director")) {
+    console.log(shuffle[0]);
+    shuffle.shift()
+  }
+  if (shuffle[0].includes("2000s")) {
+    console.log(shuffle[0]);
+    shuffle.shift()
+  }
 }
-
-
