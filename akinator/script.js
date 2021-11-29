c9b6175857b718291c74cb0b50935e98a3034a6a
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2021-11-29 16:50:02 +0100

    1

--- akinator/script.js
+++ akinator/script.js
@@ -1,6 +1,4 @@
-import {
-  movies
-} from "./movie.js";
+import { movies } from "./movie.js";
 
 const final_questions = [
   "What is the genre of your movie ?",
@@ -9,30 +7,30 @@ const final_questions = [
   "Is the director one of these proposals?",
   "Is your movie popular ?"
 ];
-let provisional_questions = final_questions;
 
-let shuffle = provisional_questions.sort((a, b, c) => 0.5 - Math.random());
-
+let shuffle = final_questions.sort((a, e) => 0.5 - Math.random());
+final_questions.
+//js copie de tableau
 
 while (!shuffle.length == []) {
   if (shuffle[0].includes("genre")) {
     console.log(shuffle[0]);
-    shuffle.shift()
+    shuffle.shift();
   }
   if (shuffle[0].includes("popular")) {
     console.log(shuffle[0]);
-    shuffle.shift()
+    shuffle.shift();
   }
   if (shuffle[0].includes("actor")) {
     console.log(shuffle[0]);
-    shuffle.shift()
+    shuffle.shift();
   }
   if (shuffle[0].includes("director")) {
     console.log(shuffle[0]);
-    shuffle.shift()
+    shuffle.shift();
   }
   if (shuffle[0].includes("2000s")) {
     console.log(shuffle[0]);
-    shuffle.shift()
+    shuffle.shift();
   }
 }
