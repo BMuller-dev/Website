2db0183d4c327b5484ac4b55286f0d40de115c69
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-12-05 14:22:17 +0100

    1

--- akinator/script.js
+++ akinator/script.js
@@ -1,36 +1,29 @@
 import { movies } from "./movie.js";
 
+function genre($x) {
+  
+}
+
+function popular($x){
+  
+  
+}
+
+
+
 const final_questions = [
-  "What is the genre of your movie ?",
-  "Is your movie from after the 2000s ?",
-  "Who is the main actor ?",
-  "Is the director one of these proposals?",
-  "Is your movie popular ?"
+  ["What is the genre of your movie ?"],
+  ["Is your movie from after the 2000s ?"],
+  ["Who is the main actor ?"],
+  ["Is the director one of these proposals?"],
+  ["Is your movie popular ?", popular()]
 ];
 
-let shuffle = final_questions.sort((a, e) => 0.5 - Math.random());
-final_questions.
-//js copie de tableau
+let shuffle = final_questions.sort(() => Math.random() - 0.5);
+alert(shuffle);
 
-while (!shuffle.length == []) {
-  if (shuffle[0].includes("genre")) {
-    console.log(shuffle[0]);
-    shuffle.shift();
-  }
-  if (shuffle[0].includes("popular")) {
-    console.log(shuffle[0]);
-    shuffle.shift();
-  }
-  if (shuffle[0].includes("actor")) {
-    console.log(shuffle[0]);
-    shuffle.shift();
-  }
-  if (shuffle[0].includes("director")) {
-    console.log(shuffle[0]);
-    shuffle.shift();
-  }
-  if (shuffle[0].includes("2000s")) {
-    console.log(shuffle[0]);
-    shuffle.shift();
-  }
-}
+shuffle.forEach(element => {
+  
+});
+
+final_questions
