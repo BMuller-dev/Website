f7c3e42767169785d80b308f6a84eac101ccd9a3
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-12-12 16:09:17 +0100

    1

--- akinator/movie.js
+++ akinator/movie.js
@@ -1,133 +1,126 @@
-let movie1 = {
-  title: "Orgueil et Préjugés",
-  releaseDate: 2005,
-  actors: [
-    { firstname: "Keira", lastname: "Matthew", birthdate: 1974 },
-    { firstname: "Knightley", lastname: "Macfayden", birthdate: 1985 }
-  ],
-  genres: ["Romance", "Drama", "History"],
-  director: "Joe Wright",
-  popular: 152,
-  question: "Is it an infamous love story ?"
-};
-let movie2 = {
-  title: "Pirates des Caraïbes",
-  releaseDate: 2005,
-  actors: [
-    { firstname: "Depp", lastname: "Johnny", birthdate: 1963 },
-    { firstname: "Knightley", lastname: "Keira", birthdate: 1985 }
-  ],
-  genre: ["Action", "Fantasy", "Adventure"],
-  director: "Gore Verbinski",
-  popular: 329,
-  question: "is it a pirate story ?"
-};
-let movie3 = {
-  title: "On a retrouvé la 7ème compagnie !",
-  director: "Robert Lamoureux",
-  releaseDate: 1975,
-  genres: ["Comedy", "War"],
-  actors: [
-    { firstname: "Jean", lastname: "Lefebre", birthdate: "1919" },
-    { firstname: "Pierre", lastname: "Mondy", birthdate: "1925" }
-  ],
-  popular: 0,
-  question: "oppss i sliped chief ?"
-};
-let movie4 = {
-  title: "La Belle et la Bête",
-  releaseDate: 2017,
-  director: "La Belle et la Bête",
-  genres: ["Family", "Fantasy", "Adventure"],
-  actors: [
-    { firstname: "Dan", lastname: "Stevens", birthdate: 1982 },
-    { firstname: "Luke Evans", lastname: "Evans", birthdate: 1988 },
-    { firstname: "Emma", lastname: "Watson", birthdate: 1979 }
-  ],
-  popular: 7,
-  question: "Is there a rose ?"
-};
-let movie5 = {
-  title: "Avengers : Endgame",
-  releaseDate: 2019,
-  director: "Joe Russo",
-  genres: ["Science fiction ", "Fantasy", "Action"],
-  actors: [
-    { firstname: "Chris", lastname: "Evans", birthdate: 1981 },
-    { firstname: "Robert", lastname: "Downey Jr", birthdate: 1965 },
-    { firstname: "Scarlett", lastname: "Johansson", birthdate: 1984 }
-  ],
-  popular: 4,
-  question: "is this movie directed by marvel ?"
-};
-let movie6 = {
-  title: "Harry Potter à l'école des sorciers",
-  director: "Chris Columbus",
-  releaseDate: 2001,
-  genres: ["Fantasy", "Adventure"],
-  actors: [
-    { firstname: "Daniel", lastname: "Radcliffe", birthdate: 1989 },
-    { firstname: "Rupert", lastname: "Grint", birthdate: 1988 },
-    { firstname: "Emma", lastname: "Watson", birthdate: 1990 }
-  ],
-  popular: 41,
-  question: "is it a magic world ? "
-};
-let movie7 = {
-  title: "Dragon Ball, Broly the super warrior ",
-  releaseDate: 2018,
-  director: "Shigeyasu Yamauchi",
-  genres: ["Fantasy", "Animation", "Adventure"],
-  actors: [
-    { firstname: "Nozawa", lastname: "Masako", birthdate: 1936 },
-    { firstname: "Bin", lastname: "Shimada", birthdate: 1954 }
-  ],
-  popular: 0,
-  question: "he is a know animator ? "
-};
-let movie8 = {
-  title: "Pearl Harbor",
-  director: "Michael Bay",
-  releaseDate: 2001,
-  genres: ["Action", "Drama", "History"],
-  actors: [
-    { firstname: "Ben", lastname: "Affleck", birthdate: 1972 },
-    { firstname: "Kate", lastname: "Beckinsale", birthdate: 1973 }
-  ],
-  popular: 1365,
-  question: "Is it a war movie ? "
-};
-let movie9 = {
-  title: "OSS 117: Rio ne répond plus",
-  releaseDate: 2009,
-  director: "Michel Hazanavicius",
-  actors: [
-    { firstname: "Jean", lastname: "Dujardin", birthdate: 1972 },
-    { firstname: "Louise", lastname: "Monot", birthdate: 1981 }
-  ],
-  genres: ["Action", "Adventure", "Comedy"],
-  popular: 0,
-  question: "Is it a French comedy movie ?"
-};
-let movie10 = {
-  title: "Princess Mononoke",
-  director: "Hayao Miyazaki",
-  releaseDate: 1997,
-  genres: ["Animation", "Action", "Adventure"],
-  actors: [],
-  popular: 1543,
-  question: "Is there a white wolf called «Moro» ?"
-};
+let movies = [
+  {
+    title: "Orgueil et Préjugés",
+    releaseDate: 2005,
+    actors: [
+      { firstname: "Keira", lastname: "Matthew", birthdate: 1974 },
+      { firstname: "Knightley", lastname: "Macfayden", birthdate: 1985 },
+    ],
+    genres: ["Romance", "Drama", "History"],
+    director: "Joe Wright",
+    popular: 152,
+    question: "Is it an infamous love story ?",
+  },
+  {
+    title: "Pirates des Caraïbes",
+    releaseDate: 2005,
+    actors: [
+      { firstname: "Depp", lastname: "Johnny", birthdate: 1963 },
+      { firstname: "Knightley", lastname: "Keira", birthdate: 1985 },
+    ],
+    genre: ["Action", "Fantasy", "Adventure"],
+    director: "Gore Verbinski",
+    popular: 329,
+    question: "is it a pirate story ?",
+  },
+  {
+    title: "On a retrouvé la 7ème compagnie !",
+    director: "Robert Lamoureux",
+    releaseDate: 1975,
+    genres: ["Comedy", "War"],
+    actors: [
+      { firstname: "Jean", lastname: "Lefebre", birthdate: "1919" },
+      { firstname: "Pierre", lastname: "Mondy", birthdate: "1925" },
+    ],
+    popular: 0,
+    question: "oppss i sliped chief ?",
+  },
+  {
+    title: "La Belle et la Bête",
+    releaseDate: 2017,
+    director: "La Belle et la Bête",
+    genres: ["Family", "Fantasy", "Adventure"],
+    actors: [
+      { firstname: "Dan", lastname: "Stevens", birthdate: 1982 },
+      { firstname: "Luke Evans", lastname: "Evans", birthdate: 1988 },
+      { firstname: "Emma", lastname: "Watson", birthdate: 1979 },
+    ],
+    popular: 7,
+    question: "Is there a rose ?",
+  },
+  {
+    title: "Avengers : Endgame",
+    releaseDate: 2019,
+    director: "Joe Russo",
+    genres: ["Science fiction ", "Fantasy", "Action"],
+    actors: [
+      { firstname: "Chris", lastname: "Evans", birthdate: 1981 },
+      { firstname: "Robert", lastname: "Downey Jr", birthdate: 1965 },
+      { firstname: "Scarlett", lastname: "Johansson", birthdate: 1984 },
+    ],
+    popular: 4,
+    question: "is this movie directed by marvel ?",
+  },
+  {
+    title: "Harry Potter à l'école des sorciers",
+    director: "Chris Columbus",
+    releaseDate: 2001,
+    genres: ["Fantasy", "Adventure"],
+    actors: [
+      { firstname: "Daniel", lastname: "Radcliffe", birthdate: 1989 },
+      { firstname: "Rupert", lastname: "Grint", birthdate: 1988 },
+      { firstname: "Emma", lastname: "Watson", birthdate: 1990 },
+    ],
+    popular: 41,
+    question: "is it a magic world ? ",
+  },
+  {
+    title: "Dragon Ball, Broly the super warrior ",
+    releaseDate: 2018,
+    director: "Shigeyasu Yamauchi",
+    genres: ["Fantasy", "Animation", "Adventure"],
+    actors: [
+      { firstname: "Nozawa", lastname: "Masako", birthdate: 1936 },
+      { firstname: "Bin", lastname: "Shimada", birthdate: 1954 },
+    ],
+    popular: 0,
+    question: "he is a know animator ? ",
+  },
+  {
+    title: "Pearl Harbor",
+    director: "Michael Bay",
+    releaseDate: 2001,
+    genres: ["Action", "Drama", "History"],
+    actors: [
+      { firstname: "Ben", lastname: "Affleck", birthdate: 1972 },
+      { firstname: "Kate", lastname: "Beckinsale", birthdate: 1973 },
+    ],
+    popular: 1365,
+    question: "Is it a war movie ? ",
+  },
+  {
+    title: "OSS 117: Rio ne répond plus",
+    releaseDate: 2009,
+    director: "Michel Hazanavicius",
+    actors: [
+      { firstname: "Jean", lastname: "Dujardin", birthdate: 1972 },
+      { firstname: "Louise", lastname: "Monot", birthdate: 1981 },
+    ],
+    genres: ["Action", "Adventure", "Comedy"],
+    popular: 0,
+    question: "Is it a French comedy movie ?",
+  },
+  {
+    title: "Princess Mononoke",
+    director: "Hayao Miyazaki",
+    releaseDate: 1997,
+    genres: ["Animation", "Action", "Adventure"],
+    actors: [],
+    popular: 1543,
+    question: "Is there a white wolf called «Moro» ?",
+  },
+];
 
-export const movies = [
-  movie1,
-  movie2,
-  movie3,
-  movie4,
-  movie5,
-  movie6,
-  movie7,
-  movie8,
-  movie9,
-  movie10
+export const list_movies = [
+  movies
 ];
--- akinator/script.js
+++ akinator/script.js
@@ -1,29 +1,55 @@
-import { movies } from "./movie.js";
-
-function genre($x) {
-  
-}
-
-function popular($x){
-  
-  
-}
-
-
+import { list_movies } from "./movie.js";
 
 const final_questions = [
-  ["What is the genre of your movie ?"],
-  ["Is your movie from after the 2000s ?"],
-  ["Who is the main actor ?"],
-  ["Is the director one of these proposals?"],
-  ["Is your movie popular ?", popular()]
+  {
+    id: 1,
+    question: "Is your movie popular ?",
+    answers: ["Yes", "No", "Don't know"],
+  },
+  {
+    id: 2,
+    question: "Who is the main actor ?",
+    answers: ["Yes", "No", "Don't know"],
+  },
+  {
+    id: 3,
+    question: "Is your movie from after the 2000s ?",
+    answers: ["Yes", "No", "Don't know"],
+  },
+  {
+    id: 4,
+    question: "Is the genre of your movie one of these proposals?",
+    answers: ["Yes", "No", "Don't know"],
+  },
+  {
+    id: 5,
+    question: "Is the director one of these proposals?",
+    answers: ["Yes", "No", "Don't know"],
+  },
+  {
+    id: 6,
+    question: "Is the main actor one of these proposals?",
+    answers: ["Yes", "No", "Don't know"],
+  },
 ];
 
+// Séléction des boutons
+let yes = document.getElementById('yes');
+let no = document.getElementById('no');
+let dont_know = document.getElementById('dont_know');
+
+// Mélangeur de question
 let shuffle = final_questions.sort(() => Math.random() - 0.5);
-alert(shuffle);
+console.log(shuffle);
 
-shuffle.forEach(element => {
-  
-});
+yes.addEventListener('click', function() {
+  console.log('test')
+})
 
-final_questions
+// test
+
+
+
+shuffle.forEach((element) => {});
+
+final_questions;
