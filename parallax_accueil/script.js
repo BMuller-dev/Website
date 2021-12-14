41c5dc5d7ac3c21c305c4dda6a15881ffadb1e4b
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-12-14 19:33:00 +0100

    1

--- parallax_accueil/script.js
+++ parallax_accueil/script.js
@@ -6,15 +6,16 @@ let text = document.getElementById('text');
 let button = document.getElementById('button');
 let header = document.querySelector('header');
 
-window.addEventListener('scroll',function(){
+window.addEventListener('scroll', function() {
     let value = window.scrollY;
 
     stars.style.left = value + 'px';
-    moon.style.top = value * 1.25  + 'px';
-    mountains_behind.style.top = value * 0.5  + 'px';
+    moon.style.top = value * 1.25 + 'px';
+    console.log(moon)
+    mountains_behind.style.top = value * 0.5 + 'px';
     mountains_front.style.top = value * 0 + 'px';
-    text.style.marginRight = value * 4  + 'px';
-    text.style.marginTop = value * 1.5  + 'px';
-    button.style.marginTop = value * 1.5  + 'px';
-    header.style.top = value * 0.5  +'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    button.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value * 0.5 + 'px';
 })
