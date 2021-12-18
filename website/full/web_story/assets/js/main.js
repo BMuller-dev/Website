837640b762ace74215e782a74b5e5dca7efc3e5c
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-12-18 13:12:56 +0100

    1

---
+++ website/full/web_story/assets/js/main.js
@@ -0,0 +1,23 @@
+function stars() {
+    let scene = document.querySelector("scene");
+
+for (let i = 0; i < 500;i++){
+        let star = document.createElement("div");
+        star.classList = "star";
+        star.textContent = '';
+        let x = Math.floor(Math.random() * window.innerHeight);
+        let y = Math.floor(Math.random() * window.innerWidth);
+        let duration = Math.random() * 10;
+        let size = Math.random() * 2;
+
+        star.style.left = x + "px";
+        star.style.top = y + "px";
+        star.style.width = 1 + size + "px";
+        star.style.height = 1 + size + "px";
+        
+        scene.appendChild(star);
+    }
+}
+
+
+stars();
--- website/full/web_story/includes/home.php
+++ website/full/web_story/includes/home.php
@@ -1,11 +1,49 @@
 <div class="scene">
-
-
-
-
-
+    <div class="night"></div>
+    <img class="forest" src="assets/img/dark_forest2.png" alt="A dark forest image">
 </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+    function stars() {
+        let scene = document.querySelector(".scene");
+
+        for (let i = 0; i < 1250; i++) {
+            let star = document.createElement("i");
+            let x = Math.floor(Math.random() * window.innerHeight);
+            let y = Math.floor(Math.random() * window.innerWidth);
+            let size = Math.random() * 2;
+
+            star.style.left = x * 2 + "px";
+            star.style.top = y * 2 + "px";
+            star.style.width = 1 + size + "px";
+            star.style.height = 1 + size + "px";
+
+            scene.appendChild(star);
+        }
+    }
+    stars();
+</script>
 
 
 
