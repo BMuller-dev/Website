5d8a5f9ec4b90a8d44fb97e1acea2acbdeeee3fe
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2021-12-22 22:04:58 +0100

    1

--- animations/heart/script.js
+++ animations/heart/script.js
@@ -5,5 +5,18 @@ heart.addEventListener("mouseover", function (event) {
 });
 
 heart.addEventListener("mouseleave", function (event) {
-  waifu.style.color = "#65001f";
+  waifu.style.color = "";
 });
+
+let feed = 1;
+let button = document.querySelector(".give_love");
+button.addEventListener("click", function () {
+  feed *= 1.02;
+  let scale = "scale(" + feed + ")";
+  heart.style.transform = scale;
+});
+
+
+
+
+
