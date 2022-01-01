4bb023e6f180fb08b02a435f38f66fff57a33552
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-01-01 03:19:13 +0100

    1

--- other/mp3/script.js
+++ other/mp3/script.js
@@ -1,33 +1,46 @@
-let player = document.querySelector(".center");
-let audio1 = new Audio("vas.mp3");
-let current = false;
-let current_timer = document.querySelector('.current-timer');
-let icon = document.querySelectorAll('.icon');
+let play = document.querySelector(".center");
+let player = new Audio("vas.mp3");
+
+let playing = false;
+let current_Time = document.querySelector('.current-timer');
 
 
-player.addEventListener("click", function () {
-temps()
-  if (current === false) {
-    audio1.play();
-    player.classList.replace('fa-play-circle', 'fa-pause-circle')
-    current = true;
-  } else {
-    audio1.pause();
-    player.classList.replace('fa-pause-circle', 'fa-play-circle')
-    current = false;
-  }
-});
-function temps() {
-    let timer = Math.floor(audio1.currentTime)
-    current_timer.innerHTML = timer
-    setTimeout("temps()",1000);
+
+
+// let progression = document.querySelector('.progression')
+// let slider = document.querySelector('.slider')
+// let slider_width = slider.clientWidth
+// let icon = document.querySelectorAll('.icon');
+
+// Timer de la musique
+function formatTime(x) {
+    let min = Math.floor(x / 60);
+    let sec = Math.floor(x % 60);
+    return min + ':' + ((sec<10) ? ('0' + sec) : sec);
 }
 
-icon.forEach(function(element) {
-    element.addEventListener("click", function(e) {
-        e.style.color = "red"
-        console.log('test')
-    })
-
-    
+// Gestion du mode play & resume
+play.addEventListener("click", function () {
+    updateProgress()
+  if (playing === false) {
+    player.play();
+    play.classList.replace('fa-play-circle', 'fa-pause-circle')
+    playing = true;
+  } else {
+    player.pause();
+    play.classList.replace('fa-pause-circle', 'fa-play-circle')
+    playing = false;
+  }
 });
+
+// Actualisation
+function updateProgress() {
+    current_Time.innerHTML = formatTime(player.currentTime)
+    setTimeout("updateProgress()",1000);
+}
+
+// let current = player.currentTime
+// let percent = (current / player.duration) * 100;
+// progression.style.marginLeft = percent + 'px'
+
+
