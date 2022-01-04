662c978fae3c4f087ccf2227406c40f42c3bb22a
BMuller-dev <71022676+BMuller-dev@users.noreply.github.com>
2022-01-04 10:44:42 +0100

    1

--- other/mp3/script.js
+++ other/mp3/script.js
@@ -7,60 +7,72 @@ let max_timer = document.querySelector(".max-timer");
 let current_timer = document.querySelector(".current-timer");
 
 let ball = document.querySelector(".ball");
+let volume_button = document.querySelector(".volume")
 
 play_pause.addEventListener("click", togglePlay);
 media.addEventListener("timeupdate", progressBar);
 
-media.addEventListener("loadedmetadata", function () {
-  max_timer.textContent = formatTime(media.duration);
+media.addEventListener("loadedmetadata", function() {
+    max_timer.textContent = formatTime(media.duration);
 });
 
+media.volume = 0
+volume_button.addEventListener("click", function() {
+    if (media.volume > 0) {
+        volume_button.classList.replace("fa-volume-up", "fa-volume-off")
+        media.volume = 0
+    } else {
+        volume_button.classList.replace("fa-volume-off", "fa-volume-up")
+        media.volume = 0.5
+    }
+})
+
+
 function formatTime(x) {
-  let min = Math.floor(x / 60);
-  let sec = Math.floor(x % 60);
-  return min + ":" + (sec < 10 ? "0" + sec : sec);
+    let min = Math.floor(x / 60);
+    let sec = Math.floor(x % 60);
+    return min + ":" + (sec < 10 ? "0" + sec : sec);
 }
 
 function percentPlayed() {
-  let slider = document.querySelector(".slider").clientWidth;
-  let ratio = slider / media.duration;
-  let percentage = (media.currentTime * ratio).toFixed(2);
-  ball.style.marginLeft = percentage + "px";
+    let slider = document.querySelector(".slider").clientWidth;
+    let ratio = slider / media.duration;
+    let percentage = (media.currentTime * ratio).toFixed(2);
+    ball.style.marginLeft = percentage + "px";
 }
 
 function progressBar() {
-  current_timer.textContent = formatTime(media.currentTime);
+    current_timer.textContent = formatTime(media.currentTime);
 
-  media.onended = () => {
-    play_pause.classList.replace("fa-pause-circle", "fa-play-circle");
-    current_timer.textContent = "00:00";
-    ball.style.marginLeft = "0px";
-  };
-  percentPlayed();
+    media.onended = () => {
+        play_pause.classList.replace("fa-pause-circle", "fa-play-circle");
+        current_timer.textContent = "00:00";
+        ball.style.marginLeft = "0px";
+    };
+    percentPlayed();
 }
 
 function togglePlay() {
-  if (media.paused === true) {
-    media.play();
-    play_pause.classList.replace("fa-play-circle", "fa-pause-circle");
-  } else {
-    media.pause();
-    play_pause.classList.replace("fa-pause-circle", "fa-play-circle");
-  }
+    if (media.paused === true) {
+        media.play();
+        play_pause.classList.replace("fa-play-circle", "fa-pause-circle");
+    } else {
+        media.pause();
+        play_pause.classList.replace("fa-pause-circle", "fa-play-circle");
+    }
 }
 
 let icon = document.querySelectorAll(".icon");
-let favorite = false;
+favorite = false
 
-icon.forEach(function(element){
-  element.addEventListener("click", function(e) {
-    if(favorite === false) {
-    e.target.style.color = "red"
-    favorite = true
-  } else {
-    e.target.style.color = "#b3b3b3"
-    favorite = false
-  }
-  })
+icon.forEach(function(element) {
+    element.addEventListener("click", function(e) {
+        if (favorite === false) {
+            e.target.classList.add("favorite")
+            favorite = true
+        } else {
+            e.target.classList.remove("favorite")
+            favorite = false
+        }
+    })
 })
-
