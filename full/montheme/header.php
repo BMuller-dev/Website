000d161260b0b7abba3c5fb4e32884a85b0847ee
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-01-16 17:04:35 +0100

    1

--- full/montheme/header.php
+++ full/montheme/header.php
@@ -9,3 +9,17 @@
 </head>
 
 <body>
+
+    <header>
+        <ul>
+            <li><a href="?page=home" class="home active">Accueil</a></li>
+            <li><a href="#" class="identity">Qui suis-je ?</a></li>
+            <li><a href="#" class="follow">Suivez-moi</a></li>
+            <li><a href="#" class="contact">Contact</a></li>
+        </ul>
+        <div class="separator">
+            <div class="line"></div>
+        </div>
+    </header>
+
+    <main>
--- full/montheme/inc/home.php
+++ full/montheme/inc/home.php
@@ -1,1 +1,16 @@
-test
+<section class="hello_world">
+    <p>"Hello World !"</p>
+    <div class="button">
+        <a class="explore">Explorons le monde</a>
+    </div>
+</section>
+
+<section class="presentation">
+    <h1>Ensemble !</h1>
+    <h2>Développeur Back-End en Freelance</h2>
+    <h3>Bryan Muller</h3>
+    <div class="social">
+        <a href="https://www.linkedin.com/in/bryan-muller-62a9b81b0" target="_blank"><i class="fab fa-linkedin"></i></a>
+        <a href="https://github.com/BMuller-dev" target="_blank"><i class="fab fa-github"></i></a>
+    </div>
+</section>
