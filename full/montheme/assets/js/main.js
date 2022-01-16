e59d62158991e4829e7609c3b7590f1536a094ca
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-01-16 23:24:16 +0100

    1

--- full/montheme/assets/js/main.js
+++ full/montheme/assets/js/main.js
@@ -1,10 +1,11 @@
-const main = document.querySelector("main");
+const body = document.querySelector("body");
 const explore = document.querySelector(".explore");
 const hello_world = document.querySelector(".hello_world");
 
 
 explore.addEventListener("click", function(){
- main.style.overflow = "visible";
+ body.style.overflow = "visible";
+ body.style.heigh = "";
  hello_world.style.animation = "transition 5s normal forwards"
  console.log('test')
 })
