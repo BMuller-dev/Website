2e0a22ac73616f062bd36de7a98fb0bdc8afb63f
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-01-25 13:18:47 +0100

    1

--- other/calculator/script.js
+++ other/calculator/script.js
@@ -1,18 +1,36 @@
 let equal = document.querySelector(".equal");
+let calcul = document.querySelectorAll(".num");
+let value = document.querySelector('.value');
+let del = document.querySelector('.del');
+
 
 equal.addEventListener("click", function () {
   let error = 0;
 
   try {
-    eval(document.calc.txt.value);
+    eval(value.value);
   } catch (e) {
     if (e instanceof SyntaxError) {
-      document.calc.txt.value = "ERROR";
+      value.value = "ERROR";
       error = 1;
     }
   }
-
   if (error != 1) {
-    document.calc.txt.value = eval(document.calc.txt.value);
+    let x = eval(value.value);
+    value.value = x.toFixed(2);
   }
 });
+
+calcul.forEach(element => {
+  element.addEventListener("click", function (e) {
+    if(value.value == 'ERROR') {
+      value.value = '';
+    }   
+    value.value += e.target.textContent
+});
+});
+
+del.addEventListener('click', function() {
+  let x = value.value
+  value.value = x.slice(0,-1); 
+})
