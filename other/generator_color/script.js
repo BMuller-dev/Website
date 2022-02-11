4d088ffae8dcf34c1add4f320604661e4537120d
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-02-12 00:47:54 +0100

    1

--- other/generator_color/script.js
+++ other/generator_color/script.js
@@ -1,34 +1,32 @@
 function color() {
     for (let i = 0; i < 100; i++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        document.querySelector('.wrap').appendChild(box);
+        const box = document.createElement("div");
+        box.classList.add("box");
+        document.querySelector(".wrap").appendChild(box);
     }
 }
 
-const button = document.querySelector('.refresh');
-const colorBox = document.querySelectorAll('.box');
-
+const button = document.querySelector(".refresh");
+const colorBox = document.querySelectorAll(".box");
 
 function randomHexColor() {
     let characters = "0123456789abcdef";
-    let colorLengh = 6;
     let color = "";
 
-    for (var i = 0; i < colorLengh; i++) {
+    for (var i = 0; i < 6; i++) {
         let randomColor = Math.floor(Math.random() * characters.length);
         color += characters.substring(randomColor, randomColor + 1);
     }
-    return "#" + color
+    return "#" + color;
 }
 
+button.addEventListener("click", addColor)
 
-
-button.addEventListener('click', function() {
+function addColor() {
     colorBox.forEach(e => {
         let newColor = randomHexColor();
         e.style.backgroundColor = newColor;
         e.innerHTML = newColor;
     });
-})
+}
 color();
